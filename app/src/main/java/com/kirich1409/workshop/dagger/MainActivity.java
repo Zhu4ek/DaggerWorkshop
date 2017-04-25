379385329b6ea08adafd4fd0ced1e373740d36a2
Kirill Rozov <kirich1409@gmail.com>
2017-04-25 13:42:04 +0300

    Add basic sample

---
+++ app/src/main/java/com/kirich1409/workshop/dagger/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kirich1409.workshop.dagger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
