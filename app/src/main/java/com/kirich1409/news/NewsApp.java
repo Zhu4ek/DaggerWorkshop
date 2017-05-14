416e3ed304f490e862eb57e3d14aee385b285fb9
Kirill Rozov <kirich1409@gmail.com>
2017-05-14 16:47:05 +0300

    Add initial project

--- app/src/main/java/com/kirich1409/news/NewsApp.java
+++ app/src/main/java/com/kirich1409/news/NewsApp.java
@@ -1,55 +1,11 @@
 package com.kirich1409.news;
 
-import android.app.Activity;
-import android.support.annotation.NonNull;
 import android.support.multidex.MultiDexApplication;
-
-import com.facebook.stetho.Stetho;
-import com.kirich1409.news.dagger.AppComponent;
-import com.kirich1409.news.dagger.AppModule;
-import com.kirich1409.news.dagger.DaggerAppComponent;
-
-import javax.inject.Inject;
-
-import dagger.android.DispatchingAndroidInjector;
-import dagger.android.HasActivityInjector;
 
 /**
  * @authror Kirill Rozov
  * @date 1/5/17.
  */
 
-public class NewsApp extends MultiDexApplication implements HasActivityInjector {
-
-    @Inject
-    DispatchingAndroidInjector<Activity> mActivityInjector;
-
-    private final AppComponent mAppComponent;
-
-    {
-        mAppComponent = DaggerAppComponent.builder()
-                .newsApiBaseUrl("https://newsapi.org/v1/")
-                .appModule(new AppModule(this))
-                .build();
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        mAppComponent.inject(this);
-        Stetho.initializeWithDefaults(this);
-    }
-
-    @NonNull
-    public final AppComponent getAppComponent() {
-        if (mAppComponent == null) {
-            throw new IllegalStateException();
-        }
-        return mAppComponent;
-    }
-
-    @Override
-    public DispatchingAndroidInjector<Activity> activityInjector() {
-        return mActivityInjector;
-    }
+public class NewsApp extends MultiDexApplication {
 }
